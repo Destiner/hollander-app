bf2f9183991ddba0e67ca91bd7ef1c86cd4c30a9
Timur Badretdinov <destinerx@gmail.com>
2022-08-31 02:25:00 +0400

    Add WalletChip to HolHeader

--- src/components/HolHeader.vue
+++ src/components/HolHeader.vue
@@ -1,24 +1,34 @@
 <template>
   <header>
-    <div>
-      <router-link :to="{ name: 'home' }">Hollander</router-link>
-      <HolButton label="Connect" />
-    </div>
+    <router-link :to="{ name: 'home' }">Hollander</router-link>
+    <HolButton
+      v-if="isConnected"
+      label="Connect"
+      @click="connect"
+    />
+    <WalletChip
+      v-else
+      :address="address"
+    />
   </header>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
 import HolButton from './HolButton.vue';
+import WalletChip from './WalletChip.vue';
+
+const address = ref('');
+const isConnected = computed(() => !address.value);
+
+function connect(): void {
+  address.value = '0x72aa5ad78fb4f2e567a5df833dad12f60b52db63';
+}
 </script>
 
 <style scoped>
 header {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-div {
   display: flex;
   align-items: center;
   justify-content: space-between;
