d6a3582882a62425c37084369c5f8edafcfd53f7
Timur Badretdinov <destinerx@gmail.com>
2022-08-30 19:03:26 +0400

    Add HolButton component

---
+++ src/components/HolButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<style scoped>
+button {
+  padding: 8px 16px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  background: var(--color-accent);
+  color: var(--color-background);
+  font-size: 14px;
+}
+
+button:hover {
+  background: var(--color-accent-lighter);
+}
+</style>
