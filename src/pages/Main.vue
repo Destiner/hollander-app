ae635c52cb397f76e44f7e914d1c34beaee9c9d4
Timur Badretdinov <destinerx@gmail.com>
2022-08-30 19:03:39 +0400

    Add hero section to Main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,3 +1,37 @@
-<template></template>
+<template>
+  <div class="page">
+    <div class="hero">
+      <div class="hero-text">
+        <div>Swap large amounts of tokens.</div>
+        <div>No MEV, low slippage.</div>
+      </div>
+      <div>
+        <HolButton label="Create" />
+      </div>
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HolButton from '@/components/HolButton.vue';
+</script>
+
+<style scoped>
+.page {
+  margin: 0 24px;
+}
+
+.hero {
+  margin: 64px 0 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.hero-text {
+  display: flex;
+  gap: 8px;
+  flex-direction: column;
+  font-size: 32px;
+}
+</style>
