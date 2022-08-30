6a85e280f527474d0a4317d810dd112e30274c74
Timur Badretdinov <destinerx@gmail.com>
2022-08-30 18:08:31 +0400

    Add HolHeader component

---
+++ src/components/HolHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="global">Hollander</div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.global {
+  display: flex;
+  align-items: center;
+  height: 64px;
+  padding: 24px;
+  font-size: 20px;
+}
+</style>
