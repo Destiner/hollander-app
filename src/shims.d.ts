913654204c6bdaa0643daa1b948682822af391cb
Timur Badretdinov <destinerx@gmail.com>
2022-09-01 20:56:56 +0400

    Provide declaration for '@metamask/jazzicon'

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,3 +1,7 @@
+declare module '@metamask/jazzicon' {
+  export default function jazzicon(size: number, seed: number): Node;
+}
+
 declare module '*.vue' {
   import { ComponentOptions } from 'vue';
 
