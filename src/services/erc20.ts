1ee85d76e08648a2a9e02d502a214201eb404435
Timur Badretdinov <destinerx@gmail.com>
2022-09-02 04:08:16 +0400

    Add ERC20 service

---
+++ src/services/erc20.ts
@@ -0,0 +1,33 @@
+import { MaxUint256 } from '@ethersproject/constants';
+import { Contract } from '@ethersproject/contracts';
+import { TransactionResponse } from '@ethersproject/providers';
+
+import EthereumService from './ethereum';
+
+import erc20Abi from '@/abi/erc20.json';
+
+class Erc20Service extends EthereumService {
+  async allowance(
+    asset: string,
+    owner: string,
+    spender: string,
+  ): Promise<bigint | null> {
+    if (!this.provider) {
+      return null;
+    }
+    const contract = new Contract(asset, erc20Abi, this.provider);
+    return await contract.allowance(owner, spender);
+  }
+
+  async approve(asset: string, spender: string): Promise<void> {
+    const signer = this.provider?.getSigner();
+    if (!signer) {
+      return;
+    }
+    const contract = new Contract(asset, erc20Abi, signer);
+    const tx: TransactionResponse = await contract.approve(spender, MaxUint256);
+    await tx.wait();
+  }
+}
+
+export default Erc20Service;
