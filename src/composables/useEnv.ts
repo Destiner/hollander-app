2c47b6b8c44d67c3bb5ea2c356b9d8961d6ac431
Timur Badretdinov <destinerx@gmail.com>
2022-09-08 10:40:53 +0400

    Add useEnv composable

---
+++ src/composables/useEnv.ts
@@ -0,0 +1,12 @@
+interface EnvironmentVariables {
+  alchemyKey: string;
+}
+
+function useEnv(): EnvironmentVariables {
+  const env = (import.meta as ImportMeta).env;
+  return {
+    alchemyKey: env.VITE_ALCHEMY_KEY || '',
+  };
+}
+
+export default useEnv;
