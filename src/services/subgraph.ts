8e0bef0986eadfc64013be16886872e79db72fc1
Timur Badretdinov <destinerx@gmail.com>
2022-09-09 20:17:00 +0400

    Add event fetching to subgraph service

--- src/services/subgraph.ts
+++ src/services/subgraph.ts
@@ -20,6 +20,48 @@ interface Auction {
   blockInit: number;
 }
 
+interface EventResponse {
+  data: {
+    inits: {
+      id: string;
+      timestamp: string;
+    }[];
+    swaps: {
+      id: string;
+      buyer: string;
+      buyAmount: string;
+      sellAmount: string;
+      timestamp: string;
+    }[];
+    withdraws: {
+      id: string;
+      amount: string;
+      timestamp: string;
+    }[];
+  };
+}
+
+interface InitEvent {
+  id: string;
+  timestamp: Date;
+}
+
+interface SwapEvent {
+  id: string;
+  buyer: string;
+  buyAmount: bigint;
+  sellAmount: bigint;
+  timestamp: Date;
+}
+
+interface WithdrawEvent {
+  id: string;
+  amount: bigint;
+  timestamp: Date;
+}
+
+type Event = InitEvent | SwapEvent | WithdrawEvent;
+
 class SubgraphService {
   async getAuctions(): Promise<Auction[]> {
     const response = await fetch(
@@ -58,8 +100,72 @@ class SubgraphService {
       };
     });
   }
+
+  async getEvents(address: string): Promise<Event[]> {
+    const response = await fetch(
+      'https://api.thegraph.com/subgraphs/name/destiner/hollander-goerli',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          query: `
+          query {
+            inits(where: { auction: "${address}" }) {
+              id
+              timestamp
+            }
+            swaps(where: { auction: "${address}" }) {
+              id
+              timestamp
+              buyer
+              buyAmount
+              sellAmount
+            }
+            withdraws(where: { auction: "${address}" }) {
+              id
+              timestamp
+            }
+          }
+        `,
+        }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      },
+    );
+
+    const json = (await response.json()) as EventResponse;
+
+    const init: InitEvent = {
+      id: json.data.inits[0].id,
+      timestamp: new Date(parseInt(json.data.inits[0].timestamp) * 1000),
+    };
+
+    const swaps: SwapEvent[] = json.data.swaps.map((swap) => {
+      const { id, buyer, buyAmount, sellAmount, timestamp } = swap;
+      return {
+        id,
+        buyer,
+        buyAmount: BigInt(buyAmount),
+        sellAmount: BigInt(sellAmount),
+        timestamp: new Date(parseInt(timestamp) * 1000),
+      };
+    });
+
+    const withdrawals: WithdrawEvent[] = json.data.withdraws.map((withdraw) => {
+      const { id, amount, timestamp } = withdraw;
+      return {
+        id,
+        amount: BigInt(amount),
+        timestamp: new Date(parseInt(timestamp) * 1000),
+      };
+    });
+
+    const events = [init, ...swaps, ...withdrawals];
+    events.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
+    return events;
+  }
 }
 
-export { Auction };
+export { Auction, Event };
 
 export default SubgraphService;
