025a2926df202b40fed7fbc7a2a7c31bd5a0d97a
Timur Badretdinov <destinerx@gmail.com>
2022-09-10 15:47:34 +0400

    Fix typing in useEnv

--- src/composables/useEnv.ts
+++ src/composables/useEnv.ts
@@ -1,9 +1,15 @@
+interface EnvMeta {
+  env: {
+    VITE_ALCHEMY_KEY: string;
+  };
+}
+
 interface EnvironmentVariables {
   alchemyKey: string;
 }
 
 function useEnv(): EnvironmentVariables {
-  const env = (import.meta as ImportMeta).env;
+  const env = (import.meta as unknown as EnvMeta).env;
   return {
     alchemyKey: env.VITE_ALCHEMY_KEY || '',
   };
